c41fb7058079f585ab0cafd07c98ea3252facd18
Grant Klopper <grant.klopper@guardian.co.uk>
2013-08-06 16:10:18 +0100

    Updated sanity tests to use www.theguardian.com

--- src/test/scala/test/AmiSanityTest.scala
+++ src/test/scala/test/AmiSanityTest.scala
@@ -9,7 +9,7 @@ class AmiSanityTest extends FlatSpec with ShouldMatchers with Http {
 
   val CacheControl = """public, max-age=(\d+)""".r
 
-  "m.guardian.co.uk" should "be on the latest version of the AMI" in {
+  "www.theguardian.com" should "be on the latest version of the AMI" in {
 
     // if this test fails you probably need to update the stack to the latest AMI (and update this test)
 
--- src/test/scala/test/FrontendSanityTest.scala
+++ src/test/scala/test/FrontendSanityTest.scala
@@ -13,10 +13,10 @@ class FrontendSanityTest extends FlatSpec with ShouldMatchers with Http {
   // caching coming through Fastly
   val FastlyCacheControl = """max-age=(\d+),  stale-while-revalidate=(\d+), stale-if-error=(\d+), private""".r
 
-  "m.guardian.co.uk" should "serve with correct headers with no gzip" in {
+  "www.theguardian.com" should "serve with correct headers with no gzip" in {
 
     val connection = GET(
-      s"http://m.guardian.co.uk/?cachebust=${currentTimeMillis}",
+      s"http://www.theguardian.com/uk?view=mobile",
       compress = false
     )
 
@@ -37,7 +37,7 @@ class FrontendSanityTest extends FlatSpec with ShouldMatchers with Http {
   it should "serve with correct headers with gzip" in {
 
     val connection = GET(
-      s"http://m.guardian.co.uk/?cachebust=${currentTimeMillis}",
+      s"http://www.theguardian.com/uk?view=mobile",
       compress = true
     )
 
@@ -50,61 +50,6 @@ class FrontendSanityTest extends FlatSpec with ShouldMatchers with Http {
       case FastlyCacheControl(maxAge, _, _) => maxAge.toInt should be > 50
       case _ =>
         println(connection.header("Cache-Control"))
-        fail("Bad cache control")
-    }
-  }
-
-  it should "compress json" in {
-    val connection = GET(
-      s"http://api.nextgen.guardianapps.co.uk/commentisfree/trails?callback=trails&cachebust=${currentTimeMillis}",
-      compress = true
-    )
-
-    connection.bodyFromGzip should include("""{"html":""")
-
-    connection.header("Vary") should include ("Accept-Encoding")
-    connection.header("Content-Type") should be ("application/javascript")
-    connection.responseCode should be (200)
-    connection.header("Cache-Control") match {
-      case L3CacheControl(maxAge, _, _, _) => maxAge.toInt should be > 50
-      case _ => fail("Bad cache control")
-    }
-  }
-
-  "m.guardiannews.com" should "serve with correct headers with no gzip" in {
-
-    val connection = GET(
-      s"http://m.guardiannews.com/?cachebust=${currentTimeMillis}",
-      compress = false
-    )
-
-    connection.body should include("The Guardian")
-
-    connection.header("Vary") should be ("Accept-Encoding,User-Agent")
-    connection.header("Content-Type") should be ("text/html; charset=utf-8")
-    connection.responseCode should be (200)
-    connection.header("Cache-Control") match {
-      case FastlyCacheControl(maxAge, _, _) => maxAge.toInt should be > 50
-      case _ => fail("Bad cache control")
-    }
-  }
-
-  it should "serve with correct headers with gzip" in {
-
-    val connection = GET(
-      s"http://m.guardiannews.com/?cachebust=${currentTimeMillis}",
-      compress = true
-    )
-
-    connection.bodyFromGzip should include("The Guardian")
-
-    connection.header("Vary") should be ("Accept-Encoding,User-Agent")
-    connection.header("Content-Type") should be ("text/html; charset=utf-8")
-    connection.responseCode should be (200)
-    connection.header("Cache-Control") match {
-      case FastlyCacheControl(maxAge, _, _) => maxAge.toInt should be > 50
-      case _ =>
-        println("|"+connection.header("Cache-Control")+"|")
         fail("Bad cache control")
     }
   }
