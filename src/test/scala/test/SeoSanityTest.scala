d13817a574b7a3e6e10b640efd4283c1de66ad80
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-04-17 11:24:36 +0100

    Add sanity test for seo/schema meta data for all content types

--- src/test/scala/test/SeoSanityTest.scala
+++ src/test/scala/test/SeoSanityTest.scala
@@ -2,73 +2,53 @@ package test
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
-
+import play.api.libs.json._
 
 class SeoSanityTest extends FlatSpec with ShouldMatchers with Http {
 
-  // caching coming through Fastly
-  val HtmlCacheControl = """max-age=(\d+), stale-while-revalidate=(\d+), stale-if-error=(\d+), private""".r
-  val AjaxCacheControl = """max-age=(\d+), stale-while-revalidate=(\d+), stale-if-error=(\d+), private""".r
-
-  "www.theguardian.com" should "serve with correct headers with no gzip" in {
-
+  def checkUrl(url: String): Unit = {
     val connection = GET(
-      s"http://www.theguardian.com/uk?view=mobile",
-      compress = false
-    )
-
-    connection.body should include("The Guardian")
-
-    connection.header("Vary") should be ("Accept-Encoding,User-Agent")
-    connection.header("Content-Type") should be ("text/html; charset=utf-8")
-    connection.header("X-GU-Platform") should be ("next-gen-router")
-    connection.responseCode should be (200)
-    connection.header("Cache-Control") match {
-      case HtmlCacheControl(maxAge, _, _) =>
-        maxAge.toInt should be > 0
-      case bad => fail("Bad cache control" + bad)
-    }
-  }
-
-  it should "serve with correct headers with gzip" in {
-
-    val connection = GET(
-      s"http://www.theguardian.com/uk?view=mobile",
-      compress = true
-    )
-
-    connection.bodyFromGzip should include("The Guardian")
-
-    connection.header("Vary") should be ("Accept-Encoding,User-Agent")
-    connection.header("Content-Type") should be ("text/html; charset=utf-8")
-    connection.header("X-GU-Platform") should be ("next-gen-router")
-    connection.responseCode should be (200)
-    connection.header("Cache-Control") match {
-      case HtmlCacheControl(maxAge, _, _) => maxAge.toInt should be > 0
-      case bad => fail("Bad cache control" + bad)
-    }
-  }
-
-  it should "compress json" in {
-    val connection = GET(
-      s"http://api.nextgen.guardianapps.co.uk/most-read/world.json",
+      s"http://linter.structured-data.org/?url=$url",
       compress = true,
       headers = Seq(
-        "Accept" -> "application/json",
-        "Origin" -> "http://www.theguardian.com"
+        "Accept" -> "application/json"
       )
     )
 
-    connection.body should include("""{"html":""")
+    val json: JsValue = Json.parse(connection.body)
 
-    connection.responseCode should be (200)
-    connection.header("Content-Type") should be ("application/json; charset=utf-8")
-    connection.header("Cache-Control") match {
-      case AjaxCacheControl(maxAge, _, _) => maxAge.toInt should be > 0
-      case bad => fail("Bad cache control" + bad)
+    val messages = (json \ "messages").as[JsArray].value
+
+    withClue(s"${messages.toSeq.mkString("\n")}\n") {
+      messages.size should be(0)
     }
-    connection.header("Vary") should be ("Accept-Encoding,Origin,Accept")
+  }
+
+  "The network front" should "serve correct and valid seo meta data" in {
+      checkUrl("http://www.theguardian.com/uk")
+  }
+
+  "Tag pages" should "serve correct and valid seo meta data" in {
+    checkUrl("http://www.theguardian.com/world/series/guardian-world-networks")
+  }
+
+  "Article pages" should "serve correct and valid seo meta data" in {
+    checkUrl("http://www.theguardian.com/politics/commentisfree/2015/apr/16/tv-opposition-leaders-debate-spin-room-westminster-artless-bullshit")
+  }
+
+  "Live blogs" should "serve correct and valid seo meta data" in {
+    checkUrl("http://www.theguardian.com/politics/live/2015/apr/17/election-2015-live-ed-miliband-nicola-sturgeon-david-cameron-snp-labour")
+  }
+
+  "Gallery pages" should "serve correct and valid seo meta data" in {
+    checkUrl("http://www.theguardian.com/artanddesign/gallery/2015/apr/15/postcards-from-the-ruins-the-desolate-edge-of-eastern-europe-tamas-dezso-in-pictures")
+  }
+
+  "Video pages" should "serve correct and valid seo meta data" in {
+    checkUrl("http://www.theguardian.com/lifeandstyle/video/2015/apr/17/motivation-finish-studies-problem-agony-aunt-video")
+  }
+
+  "Interactive pages" should "serve correct and valid seo meta data" in {
+    checkUrl("http://www.theguardian.com/environment/ng-interactive/2015/apr/16/gates-foundation-wellcome-trust-climate-change-divest-fossil-fuels-guardian")
   }
 }
-
-
