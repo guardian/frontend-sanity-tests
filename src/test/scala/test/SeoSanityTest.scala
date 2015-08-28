073949c7dfbe1ce5ad1b1ce3035370d6abcd589b
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-08-28 16:14:47 +0100

    Add live blog schema tests

--- src/test/scala/test/SeoSanityTest.scala
+++ src/test/scala/test/SeoSanityTest.scala
@@ -67,20 +67,7 @@ class SeoSanityTest extends FlatSpec with Matchers with Http with OptionValues {
   }
 
   "Live blogs" should "serve correct and valid seo meta data" in {
-    checkUrl("http://www.theguardian.com/football/live/2015/apr/18/reading-arsenal-fa-cup-semi-final-live", {
-      // this is a temp hack as LiveBlogPosting is not on schema.org yet. Please remove if http://schema.org/LiveBlogPosting is there
-      messages: Seq[String] =>
-        messages.filter {
-              case "class schema:LiveBlogPosting: No class definition found" => false
-              case "property schema:liveBlogUpdate: No property definition found" => false
-              case "property schema:coverageStartTime: No property definition found" => false
-              case "property schema:coverageEndTime: No property definition found" => false
-              case _ => true
-            }
-        }
-    )
-    // when this line fails, it's time to remove the above filters
-    GET("http://schema.org/LiveBlogPosting").responseCode should be(404)
+    checkUrl("http://www.theguardian.com/football/live/2015/apr/18/reading-arsenal-fa-cup-semi-final-live")
   }
 
   "Gallery pages" should "serve correct and valid seo meta data" in {
